d776e3b431a4ab3ad141d51219cff6d8eef8bdb8
Yifu Qiao <yifugo@gmail.com>
2019-07-31 11:18:13 -0700

    Revert "tes"
    This reverts commit 0ef92c445d350b75fa2120f740c8e3df84acb12c.

--- RoadFighter2D/Assets/Scripts/PosNetworkSync.cs
+++ RoadFighter2D/Assets/Scripts/PosNetworkSync.cs
@@ -6,28 +6,17 @@ using Photon.Pun;
 public class PosNetworkSync : MonoBehaviourPun, IPunObservable
 {
     private float m_xPos = 0f;
-    private bool m_bIsCalibrating = false;
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
 
         if (stream.IsWriting)
         {
             stream.SendNext(m_xPos);
-            if (m_bIsCalibrating)
-            {
-                stream.SendNext(transform.parent.position.y);
-            }
         }
         else
         {
             m_xPos = (float)stream.ReceiveNext();
             transform.localPosition = new Vector3(m_xPos,transform.localPosition.y, transform.localPosition.z);
-            if (m_bIsCalibrating)
-            {
-                var yPos = (float)stream.ReceiveNext();
-                transform.parent.position = new Vector3(transform.parent.position.x, yPos, transform.parent.position.z);
-            }
-
         }
     }
 
@@ -50,7 +39,6 @@ public class PosNetworkSync : MonoBehaviourPun, IPunObservable
         {
             photonView.RPC("HitACar", RpcTarget.OthersBuffered);
             PlayerControl.Instance.OnCollidedWithCar();
-            m_bIsCalibrating = true;
         }
     }
 
